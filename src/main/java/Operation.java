04cd2aaca9f1ee26c2cb48fe5ee8f2ecc14952e2
handris <andrashinkel@gmail.com>
2018-07-27 20:39:14 +0200

    Initial commit

---
+++ src/main/java/Operation.java
@@ -0,0 +1,12 @@
+import java.math.BigDecimal;
+
+
+public class Operation {
+    public static BigDecimal add(BigDecimal x, BigDecimal y) {
+        return x.add(y);
+    }
+
+    public static BigDecimal substract(BigDecimal x, BigDecimal y) {
+        return x.subtract(y);
+    }
+}
